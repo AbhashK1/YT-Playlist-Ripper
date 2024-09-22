e1f9038d685bb4ee87c5e5a3d793b14f818a6991
Abhash Kumar <abhash.kr99@gmail.com>
2024-09-22 21:47:28 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,7 +4,6 @@ from tkinter import *
 from yt_dlp import YoutubeDL
 import subprocess
 
-# Create a threading event to signal the end of the download
 download_complete = threading.Event()
 
 def download_playlist():
@@ -23,28 +22,30 @@ def download_playlist():
             }
             with YoutubeDL(ydl_opts) as ydl:
                 ydl.download([playlist_url])
-            # Signal that the download is complete
+
             download_complete.set()
         except Exception as e:
-            # Handle any exceptions and stop the spinner
+
             stop_spinner()
             status_label.config(text=f"Error: {str(e)}")
             download_complete.set()
 
-    # Start the spinner and clear any previous error messages
     status_label.config(text="")
-    download_complete.clear()  # Reset the event
+    download_complete.clear()
     start_spinner()
     threading.Thread(target=run_download).start()
+
 
 def start_spinner():
     global spinner_running
     spinner_running = True
     update_spinner()
 
+
 def stop_spinner():
     global spinner_running
     spinner_running = False
+
 
 def update_spinner():
     if spinner_running:
@@ -53,27 +54,27 @@ def update_spinner():
             status_label.config(text=current_text + ".")
         else:
             status_label.config(text="")
-        # Continue the spinner until the download is complete
+
         if not download_complete.is_set():
             root.after(500, update_spinner)
         else:
-            # Stop spinner and update the label when download is complete
+
             stop_spinner()
             status_label.config(text="Download Complete")
             open_button.config(state=NORMAL)
 
 def open_downloads_folder():
     folder_path = os.path.join(os.getcwd(), 'downloads')
-    if os.name == 'nt':  # Windows
+    if os.name == 'nt':
         subprocess.Popen(f'explorer "{folder_path}"')
-    elif os.name == 'posix':  # macOS or Linux
+    elif os.name == 'posix':
         subprocess.Popen(['open', folder_path])
 
-# Set up the GUI
+
 root = Tk()
 root.title('YouTube Playlist Downloader')
 
-# Set the custom icon (optional)
+
 root.iconbitmap('download.ico')
 
 url_label = Label(root, text='Enter YouTube Playlist URL:')
